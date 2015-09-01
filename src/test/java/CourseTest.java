1e10229502ca88e6f63cd035e39fff94637b6304
teresa fletcher & jocelyn alsdorf <student@epicodus.com>
2015-09-01 11:51:12 -0700

    pass empty, override an save Course unit tests

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,33 +1,33 @@
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import org.junit.Rule;
-//
-//
-// public class CourseTest {
-//
-//   @Rule
-//   public DatabaseRule database = new DatabaseRule();
-//
-//   @Test
-//   public void all_emptyAtFirst() {
-//     assertEquals(Task.all().size(), 0);
-//   }
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Rule;
 
-  // @Test
-  // public void equals_returnsTrueIfDescriptionsAretheSame() {
-  //   Task firstTask = new Task("Mow the lawn");
-  //   Task secondTask = new Task("Mow the lawn");
-  //   assertTrue(firstTask.equals(secondTask));
-  // }
-  //
-  // @Test
-  // public void save_savesObjectIntoDatabase() {
-  //   Task myTask = new Task("Mow the lawn");
-  //   myTask.save();
-  //   Task savedTask = Task.all().get(0);
-  //   assertTrue(savedTask.equals(myTask));
-  // }
-  //
+
+public class CourseTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Course.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Course firstCourse = new Course("Mow the lawn", "1");
+    Course secondCourse = new Course("Mow the lawn", "1");
+    assertTrue(firstCourse.equals(secondCourse));
+  }
+
+  @Test
+  public void save_savesObjectIntoDatabase() {
+    Course myCourse = new Course("Mow the lawn", "1");
+    myCourse.save();
+    Course savedCourse = Course.all().get(0);
+    assertTrue(savedCourse.equals(myCourse));
+  }
+
   // @Test
   // public void save_assignsIdToObject() {
   //   Task myTask = new Task("Mow the lawn");
@@ -43,4 +43,4 @@
   //   Task savedTask = Task.find(myTask.getId());
   //   assertTrue(myTask.equals(savedTask));
   // }
-//}
+}
