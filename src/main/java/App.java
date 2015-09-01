d2d37f148ca1baa2b4aaa4141c060a8e8817dab3
teresa fletcher & jocelyn alsdorf <student@epicodus.com>
2015-09-01 14:41:32 -0700

    add routes for courses and students vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,6 +16,32 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/courses", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Course> courses = Course.all();
+      model.put("courses", courses);
+      model.put("template", "templates/courses.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/students", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Student> students = Student.all();
+      model.put("students", students);
+      model.put("template", "templates/students.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/courses/:id", (request, reponse) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Course course = Course.find(id);
+      model.put("course", course);
+      model.put("allStudents", Student.all());
+      model.put("template","templates/course.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
   }
 }
